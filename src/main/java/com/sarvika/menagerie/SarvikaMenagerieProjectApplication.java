3ea7a85dbbcbc99683fc45389eeea765f37d3146
sanjay <khardiyasanjay065@gmail.com>
2024-09-25 17:30:22 +0530

    Initial commit

---
+++ src/main/java/com/sarvika/menagerie/SarvikaMenagerieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sarvika.menagerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarvikaMenagerieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarvikaMenagerieProjectApplication.class, args);
+	}
+
+}
